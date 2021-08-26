9863ae0f0e4e674752c2e49dc5216d5799267a3a
harshmsomani <harshmsomani@outlook.com>
2021-08-26 17:20:41 +0530

    Function for retiming raw sensor data

--- bgRetimeData.m
+++ bgRetimeData.m
@@ -1,11 +1,16 @@
 function [dataRetime,dataSeconds] = bgRetimeData(data)
-%bgRetimeData Summary of this function goes here
-%   Detailed explanation goes here
+%bgRetimeData return retimed and secondly data
+%   User inputs raw data as a time table object. The repeated time stamp
+%   values are retimed and returned along with secondly data. 
 
+%Find unique rows in data
 uniqueRowsTT = unique(data);
+%Find unique time stamps in data
 uniqueTimes = unique(uniqueRowsTT.time);
 
+%Retime data to include mean values of Voltage for repeated time stamps
 dataRetime = retime(uniqueRowsTT,uniqueTimes,'mean');
+%Retime data to average values on a per second scale
 dataSeconds = retime(dataRetime,'secondly','mean');
 end
 
